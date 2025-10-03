b27b9a16c24e4b7eca3aa3a6efa76a0f3c35d459
veerpclemson <veerp@clemson.edu>
2025-10-03 11:37:16 -0400

    implemented data fetching for player usage from 2021-2024

---
+++ backend/app/data/etl/fetch_player_usage.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_URI = os.getenv("DATABASE_URL")
+engine = create_engine(DB_URI)
+#store all datasets in heere per year
+dfs = []
+
+for i in range(2021, 2025):
+    url = f"https://www.pro-football-reference.com/years/{i}/fantasy.htm"
+    tables = pd.read_html(url, header=1)
+    usage_df = tables[0]
+    
+    usage_df.columns = usage_df.columns.get_level_values(0)
+    
+    usage_df = usage_df[usage_df['Rk'] != 'Rk'].copy()  # Make an explicit copy
+    usage_df.loc[:, "season"] = i
+
+    
+    usage_df = usage_df.reset_index(drop=True)
+     
+
+    dfs.append(usage_df)
+#merge them all into one dataset
+usage_all = pd.concat(dfs, ignore_index=True)
+
+usage_all.to_sql("player_usage", engine, if_exists = "replace", index = False)
